37d426979b9b2223a4c33bca3c3c6a59cb589f9f
Felipe Tanios <felipe.tanios@gmail.com>
2017-11-10 14:05:51 -0200

    added the first attemp to create a server with ESP8266

--- Servidor_ESP8266/Servidor_ESP8266.ino
+++ Servidor_ESP8266/Servidor_ESP8266.ino
@@ -7,7 +7,7 @@ SoftwareSerial esp8266(2, 3);
 
 String sendData(String command, const int timeout, boolean debug = false)
 {
-  // Envio dos comandos AT para o modulo
+  // Send AT commands
   String response = "";
   esp8266.print(command);
   long int time = millis();
@@ -29,18 +29,43 @@ String sendData(String command, const int timeout, boolean debug = false)
 
 void setup() {
   String test_return;
-  delay(1000);
   
   Serial.begin(9600);
-  //esp8266 anteriormente mudado para baudrate = 19200
+  
+  //esp8266  baudrate previously changed to 19200 (standard is 115200)
   esp8266.begin(19200);
-  sendData("ATE1\r\n", 2000, DEBUG); // set echo to sent message
-  delay(2000);
-  sendData("AT\r\n", 2000, DEBUG); // AT just to recieve OK
-  delay(2000);
-  test_return = sendData("AT+GMR\r\n", 2000); // recieves version just for testing
+  
+  sendData("AT+RST\r\n", 2000, DEBUG); // rst
+  delay(500);
+  // set echo to print the sent message
+  sendData("ATE1\r\n", 2000, DEBUG); 
+  delay(500);
+  // recieves version just for testing
+  test_return = sendData("AT+GMR\r\n", 2000); 
   delay(2000);
   Serial.println(test_return);
+  
+  
+  //trying to connecto to internet
+  //show all the access points available
+  sendData("AT+CWLAP\r\n", 2000, DEBUG);
+  //connect to wifi
+  //AT command = AT+CWJAP="SSID","password" 
+  sendData("AT+CWJAP=\"Vanilda\",\"1234567890123\"\r\n", 2000, DEBUG);
+  delay(3000);
+  //test connection
+  sendData("AT+CWJAP?\r\n", 2000, DEBUG);
+  delay(2000);
+  //changes to server mode
+  delay(3000);
+  sendData("AT+CWMODE=1\r\n", 1000, DEBUG);
+  // Shows IP adress
+  sendData("AT+CIFSR\r\n", 1000, DEBUG);
+  // Sets the server to multiple connections
+  sendData("AT+CIPMUX=1rn", 1000, DEBUG);
+  // Starts web server at port 80
+  sendData("AT+CIPSERVER=1,80rn", 1000, DEBUG);
+  
   Serial.println("FIM");
 }
 
@@ -48,3 +73,63 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+ 
+//void loop()
+//{
+//  // Verifica se o ESP8266 esta enviando dados
+//  if (esp8266.available())
+//  {
+//    if (esp8266.find("+IPD,"))
+//    {
+//      delay(300);
+//      int connectionId = esp8266.read() - 48;
+// 
+//      String webpage = "<head><meta http-equiv=""refresh"" content=""3"">";
+//      webpage += "</head><h1><u>ESP8266 - Web Server</u></h1><h2>Porta";
+//      webpage += "Digital 8: ";
+//      int a = digitalRead(8);
+//      webpage += a;
+//      webpage += "<h2>Porta Digital 9: ";
+//      int b = digitalRead(9);
+//      webpage += b;
+//      webpage += "</h2>";
+// 
+//      String cipSend = "AT+CIPSEND=";
+//      cipSend += connectionId;
+//      cipSend += ",";
+//      cipSend += webpage.length();
+//      cipSend += "rn";
+// 
+//      sendData(cipSend, 1000, DEBUG);
+//      sendData(webpage, 1000, DEBUG);
+// 
+//      String closeCommand = "AT+CIPCLOSE=";
+//      closeCommand += connectionId; // append connection id
+//      closeCommand += "rn";
+// 
+//      sendData(closeCommand, 3000, DEBUG);
+//    }
+//  }
+//}
+// 
+//String sendData(String command, const int timeout, boolean debug)
+//{
+//  // Envio dos comandos AT para o modulo
+//  String response = "";
+//  esp8266.print(command);
+//  long int time = millis();
+//  while ( (time + timeout) > millis())
+//  {
+//    while (esp8266.available())
+//    {
+//      // The esp has data so display its output to the serial window
+//      char c = esp8266.read(); // read the next character.
+//      response += c;
+//    }
+//  }
+//  if (debug)
+//  {
+//    Serial.print(response);
+//  }
+//  return response;
+//}
