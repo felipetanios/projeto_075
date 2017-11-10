453b09870d7b7634d2ade82c502d3e6e33c587dc
Felipe Tanios <felipe.tanios@gmail.com>
2017-11-10 18:38:02 -0200

    server configuration set up
    now we need to set up a webpage at this server. All code is commented, i believe it's easy to understand

--- Servidor_ESP8266/Servidor_ESP8266.ino
+++ Servidor_ESP8266/Servidor_ESP8266.ino
@@ -5,22 +5,20 @@ SoftwareSerial esp8266(2, 3);
 
 #define DEBUG true
 
-String sendData(String command, const int timeout, boolean debug = false)
-{
+String IP = "";
+
+String sendData(String command, const int timeout, boolean debug = false){
   // Send AT commands
   String response = "";
   esp8266.print(command);
   long int time = millis();
-  while ( (time + timeout) > millis())
-  {
-    while (esp8266.available())
-    {
+  while ( (time + timeout) > millis()){
+    while (esp8266.available()){
       char c = esp8266.read(); // read the next character.
       response += c;
     }
   }
-  if (debug)
-  {
+  if (debug){
     Serial.println(response);
   }
   return response;
@@ -29,44 +27,59 @@ String sendData(String command, const int timeout, boolean debug = false)
 
 void setup() {
   String test_return;
+  String flag_wifi;
   
   Serial.begin(9600);
   
   //esp8266  baudrate previously changed to 19200 (standard is 115200)
   esp8266.begin(19200);
   
-  sendData("AT+RST\r\n", 2000, DEBUG); // rst
-  delay(500);
-  // set echo to print the sent message
-  sendData("ATE1\r\n", 2000, DEBUG); 
+
+  Serial.println("testing ESP8266");
+  //enable echo
+  sendData("ATE1\r\n", 2000); 
   delay(500);
   // recieves version just for testing
   test_return = sendData("AT+GMR\r\n", 2000); 
   delay(2000);
   Serial.println(test_return);
   
-  
-  //trying to connecto to internet
-  //show all the access points available
-  sendData("AT+CWLAP\r\n", 2000, DEBUG);
-  //connect to wifi
-  //AT command = AT+CWJAP="SSID","password" 
-  sendData("AT+CWJAP=\"Vanilda\",\"1234567890123\"\r\n", 2000, DEBUG);
-  delay(3000);
-  //test connection
-  sendData("AT+CWJAP?\r\n", 2000, DEBUG);
-  delay(2000);
+
+  Serial.println("Setting up connection");
+ 
+  delay(500);
   //changes to server mode
-  delay(3000);
+  Serial.println("Changing to server mode");
   sendData("AT+CWMODE=1\r\n", 1000, DEBUG);
-  // Shows IP adress
+  //check connection
+  Serial.println("Checking connection");
+  flag_wifi = sendData("AT+CWJAP?\r\n", 2000);
+  delay(2000);
+  //if answer is No AP (it's not connected)
+  if (flag_wifi[0] == 'N'){
+    Serial.println("Not connected to any AP. Trying to connect");
+    //trying to connecto to internet
+    //connect to wifi
+    //AT command = AT+CWJAP="SSID","password"
+    //this part is messy, but if it does not work, you can set it up by hand through arduino tx/rx serial connection
+    sendData("AT+CWJAP=\"Vanilda\",\"1234567890123\"\r\n", 2000, DEBUG);
+    delay(2000);
+  }
+  //when its connected
+  Serial.println("Connected");
+    // Shows IP adress
+  Serial.println("Checking IP");
   sendData("AT+CIFSR\r\n", 1000, DEBUG);
   // Sets the server to multiple connections
-  sendData("AT+CIPMUX=1rn", 1000, DEBUG);
+  Serial.println("Setting Server to multiple connections");
+  sendData("AT+CIPMUX=1\r\n", 1000, DEBUG);
   // Starts web server at port 80
-  sendData("AT+CIPSERVER=1,80rn", 1000, DEBUG);
-  
-  Serial.println("FIM");
+  Serial.println("Starting server at port 80");
+  sendData("AT+CIPSERVER=1,80\r\n", 1000, DEBUG);
+
+  //disable echo
+  sendData("ATE0\r\n", 2000);
+  Serial.println("End of setting up");
 }
 
 void loop() {
